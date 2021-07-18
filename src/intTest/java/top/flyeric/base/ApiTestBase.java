da763c06e3ac5dbdb62b1efea600b0fbb677cda8
liangbo <bo.liang@thoughtworks.com>
2021-07-18 22:39:32 +0800

    fix: integration test acess database issue

--- src/intTest/java/top/flyeric/base/ApiTestBase.java
+++ src/intTest/java/top/flyeric/base/ApiTestBase.java
@@ -1,31 +1,23 @@
 package top.flyeric.base;
 
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
-import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 
-import org.apache.catalina.security.SecurityConfig;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import top.flyeric.util.YamlDataSetLoader;
 
-@Import({DbTestConfig.class, SecurityConfig.class, MockConfig.class})
-@AutoConfigureTestDatabase(replace = NONE)
-@DbUnitConfiguration(databaseConnection = "dbUnitDatabaseConnection", dataSetLoader = YamlDataSetLoader.class)
-@TestExecutionListeners(
-        value = {TransactionDbUnitTestExecutionListener.class, MockitoTestExecutionListener.class},
-        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+// @Import({DbTestConfig.class, SecurityConfig.class, MockConfig.class})
+// @AutoConfigureTestDatabase(replace = NONE)
+// @DbUnitConfiguration(databaseConnection = "dbUnitDatabaseConnection", dataSetLoader =
+// YamlDataSetLoader.class)
+// @TestExecutionListeners(
+// value = {TransactionDbUnitTestExecutionListener.class, MockitoTestExecutionListener.class},
+// mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class ApiTestBase {
--- src/intTest/java/top/flyeric/base/DbTestBase.java
+++ src/intTest/java/top/flyeric/base/DbTestBase.java
@@ -1,35 +1,18 @@
 package top.flyeric.base;
 
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
-
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DbUnitConfiguration;
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-
-import top.flyeric.util.YamlDataSetLoader;
-
-@Import({DbTestConfig.class})
-@TestExecutionListeners(
-        value = {DependencyInjectionTestExecutionListener.class,
-                DirtiesContextTestExecutionListener.class,
-                TransactionalTestExecutionListener.class,
-                DbUnitTestExecutionListener.class},
-        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
-@DbUnitConfiguration(databaseConnection = "dbUnitDatabaseConnection", dataSetLoader = YamlDataSetLoader.class)
-@AutoConfigureTestDatabase(replace = NONE)
-@DataJpaTest
-@RunWith(SpringRunner.class)
-@ActiveProfiles("test")
+// @Import({DbTestConfig.class})
+// @TestExecutionListeners(
+// value = {DependencyInjectionTestExecutionListener.class,
+// DirtiesContextTestExecutionListener.class,
+// TransactionalTestExecutionListener.class,
+// DbUnitTestExecutionListener.class},
+// mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+// @DbUnitConfiguration(databaseConnection = "dbUnitDatabaseConnection", dataSetLoader =
+// YamlDataSetLoader.class)
+// @AutoConfigureTestDatabase(replace = NONE)
+// @DataJpaTest
+// @RunWith(SpringRunner.class)
+// @ActiveProfiles("test")
 public abstract class DbTestBase {
 
 }
