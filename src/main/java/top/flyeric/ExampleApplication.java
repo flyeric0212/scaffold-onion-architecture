0e4e82c56e6837b1456db0c8eb0e84c2327b95b1
liangbo <bo.liang@thoughtworks.com>
2021-07-18 22:24:37 +0800

    tech: remove mysql and add test log

--- src/main/java/top/flyeric/ExampleApplication.java
+++ src/main/java/top/flyeric/ExampleApplication.java
@@ -1,13 +1,33 @@
 package top.flyeric;
 
+import static top.flyeric.base.exception.NotFoundException.notFoundException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class ExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @Scheduled(fixedRate = 5000L)
+    public void sayHelloLog() {
+        log.info("hello, {}", LocalDateTime.now());
+    }
+
+    @Scheduled(fixedRate = 10000L)
+    public void throwExceptionLog() {
+        throw notFoundException(String.format("not found exception, %s", LocalDateTime.now())).get();
     }
 
 }
