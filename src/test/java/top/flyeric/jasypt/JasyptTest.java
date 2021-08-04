5d01ce5af9e1a07f76cc7fb0dbb86db9bf717654
liangbo <bo.liang@thoughtworks.com>
2021-08-04 21:36:39 +0800

    update spring application yamls

---
+++ src/test/java/top/flyeric/jasypt/JasyptTest.java
@@ -0,0 +1,26 @@
+package top.flyeric.jasypt;
+
+import com.ulisesbocchio.jasyptspringboot.configuration.StringEncryptorBuilder;
+import com.ulisesbocchio.jasyptspringboot.properties.JasyptEncryptorConfigurationProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JasyptTest {
+
+    @Test
+    public void encrypt() {
+        JasyptEncryptorConfigurationProperties properties = new JasyptEncryptorConfigurationProperties();
+        properties.setPassword("abc!23");
+        StringEncryptor encryptor = new StringEncryptorBuilder(properties, "").build();
+
+        String originString = "test";
+        String encryptedString = encryptor.encrypt(originString);
+        System.out.println("encryptedString : " + encryptedString);
+
+        String decryptString = encryptor.decrypt(encryptedString);
+        assertThat(decryptString).isEqualTo(originString);
+    }
+
+}
